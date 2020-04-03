01ec13cb047d653e2ae880b76582dcfd22744b66
neet-lord <lusayohendy@gmail.com>
2020-04-03 12:51:59 +0000

    Changed the ordering rule select to follow the design pattern.

--- static/site_assets/js/common/events.js
+++ static/site_assets/js/common/events.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.back-button').on('click', function() {
         history.back();
     });
-    
+
     $(window).scroll( function(event) {
         if(window.scrollY > 50) {
             document.getElementById('back-to-top-div').classList.remove('d-none');
@@ -10,32 +10,41 @@ $(document).ready(function() {
             document.getElementById('back-to-top-div').classList.add('d-none');
         }
     });
-    
+
     var watchlist_toggler = $('[data-toggle="watchlist_state"]');
-    
+
     if(watchlist_toggler)
         toggle_watchlist_state_button(watchlist_toggler);
-    
+
     var bookmark_toggler = $('[data-toggle="bookmark_state"]');
-    
+
     if (bookmark_toggler)
         toggle_bookmark_state_button(bookmark_toggler);
-    
+
     $('[data-toggle="enable_inputs"]').on('click', function(event) {
         var button = event.currentTarget;
         var targets = button.getAttribute('data-targets');
         targets = targets.split(' ');
-    
+
         enable_edit_user_details(button, targets);
     });
-    
+
     $('[data-toggle="apply_filters"]').on('click', function(event) {
         var button = event.currentTarget;
-    
+
         var filterable_section = button.getAttribute('data-filterable-section');
         var items_per_page = button.getAttribute('data-items-per-page');
-    
+
         apply_active_filters(filterable_section, items_per_page);
+    });
+
+    $('[data-toggle="apply_ordering_rule"]').on('change', function(event) {
+       var select = event.currentTarget;
+
+       var filterable_section = select.getAttribute('data-filterable-section');
+       var items_per_page = select.getAttribute('data-items-per-page');
+
+       apply_ordering_rule(filterable_section, items_per_page);
     });
 
     $('[data-action="rate_manga"]').on('click', function(event) {
@@ -46,7 +55,7 @@ $(document).ready(function() {
 
         submit_rating(button, manga_id, rating);
     });
-    
+
     $.each($('[data-action="get_bookmarked"]'), function(index, element) {
         var manga_id = element.getAttribute('data-manga');
 
