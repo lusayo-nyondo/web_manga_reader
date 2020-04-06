d8a8a08c2dd81f303458c617b9866ee18fc85388
neet-lord <lusayohendy@gmail.com>
2020-04-06 10:45:56 +0000

    Modified all watchlist references to library.

--- user_manga_integration/views/watchlist/json.py
+++ user_manga_integration/views/watchlist/json.py
@@ -23,24 +23,24 @@ def add_manga_to_user_watchlist(request):
         if is_manga_in_user_watchlist:
             response = {
                 'status': 'success',
-                'description': 'The manga is already in your watchlist.',
+                'description': 'The manga is already in your library.',
             }
         else:
             success = user_watchlist.add_manga_to_user_watchlist(user, manga)
-        
+
             if success:
                 response = {
                     'status': 'success',
-                    'description': 'The manga has been added to your watchlist successfully.',
+                    'description': 'The manga has been added to your library successfully.',
                 }
             else:
                 response = {
                     'status': 'failed',
-                    'description': 'The manga has not been added to your watchlist.',
+                    'description': 'The manga has not been added to your library. If this problem persists record the time and notify the developers.',
                 }
     else:
         response = request_validation
-    
+
     return JsonResponse(response, safe=False)
 
 def is_manga_in_user_watchlist(request):
@@ -51,18 +51,18 @@ def is_manga_in_user_watchlist(request):
         manga = request_validation['manga']
 
         is_manga_in_user_watchlist = user_watchlist.is_manga_in_user_watchlist(user, manga)
-    
+
         if is_manga_in_user_watchlist:
             response = {
                 'status': 'success',
                 'is_manga_in_user_watchlist': True,
-                'description': 'The manga has been found in the user\'s watchlist.',
+                'description': 'The manga has been found in the user\'s library.',
             }
         else:
             response = {
                 'status': 'success',
                 'is_manga_in_user_watchlist': False,
-                'description': 'The manga has not been found in the user\'s watchlist.',
+                'description': 'The manga has not been found in the user\'s library.',
             }
     else:
         response = request_validation
@@ -82,16 +82,16 @@ def remove_manga_from_user_watchlist(request):
         if success:
             response = {
                 'status': 'success',
-                'description': 'The manga has been removed from your watchlist',
+                'description': 'The manga has been removed from your library',
             }
         else:
             response = {
                 'status': 'failed',
-                'description': 'This manga wasn\'t in your watchlist.',
+                'description': 'This manga wasn\'t in your library.',
             }
     else:
         response = request_validation
 
     return JsonResponse(response, safe=False)
- 
+
 ### END MANGA WATCHLIST HANDLERS
