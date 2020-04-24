18cd206ca162bfee30acb2cdd0e76c095466cc80
neet-lord <lusayohendy@gmail.com>
2020-04-24 17:12:47 +0200

    Started the social integration app.

---
+++ social_integration/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SocialIntegrationConfig(AppConfig):
+    name = 'social_integration'
---
+++ social_integration/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
