d87e2d1498c7502d3e4dc9b9d3446a6a58799c75
neet-lord <lusayohendy@gmail.com>
2020-09-19 12:02:07 +0200

    Fixed WSGI finally.

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/clickmanga/webapp/mangapoint')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
